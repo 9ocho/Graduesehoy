8897ee61c02d0207e16568fa0f46e6a9e1832e3b
Juanvc24 <61165748+Juanvc24@users.noreply.github.com>
2020-04-01 10:59:49 -0500

    Add files via upload

---
+++ armarlo/ingredientes.php
@@ -0,0 +1,21 @@
+<?php
+    require("connect_db.php");
+    $sql=("SELECT * FROM ingredientes");
+    $query=mysqli_query($mysqli,$sql);
+    $cont= mysqli_num_rows($query);
+    //Ingredientes es un array vacio en que se van a guardar todos los dartos de la consulta
+    $ingredientes = array();
+    while($dato=mysqli_fetch_array($query)) {
+        //por cada resultado de la consulta se crea una nueva posicion en el array con todas las columnas y su valor
+        $ingredientes[] = array(
+            'id'     => $dato['id'],
+            'nombre' => $dato['nombre'],
+            'precio' => $dato['precio'],
+            'img'    => $dato['img']
+        );
+    }
+    //Se retorna a la funcion success del ajax con un echo y se pasa de array a formato texto para pdoerlo 
+    //transferir al frontend, donde se hace el proceso contrario al recibirlo, osea volverlo una estructura de datos
+    //de nuevo 
+    echo json_encode($ingredientes);
+?>
---
+++ armarlo/panes.php
@@ -0,0 +1,21 @@
+<?php
+    require("connect_db.php");
+    $sql=("SELECT * FROM panes");
+    $query=mysqli_query($mysqli,$sql);
+    $cont= mysqli_num_rows($query);
+    //Ingredientes es un array vacio en que se van a guardar todos los dartos de la consulta
+    $panes = array();
+    while($dato=mysqli_fetch_array($query)) {
+        //por cada resultado de la consulta se crea una nueva posicion en el array con todas las columnas y su valor
+        $panes[] = array(
+            'id'     => $dato['id'],
+            'nombre' => $dato['nombre'],
+            'precio' => $dato['precio'],
+            'img'    => $dato['img']
+        );
+    }
+    //Se retorna a la funcion success del ajax con un echo y se pasa de array a formato texto para pdoerlo 
+    //transferir al frontend, donde se hace el proceso contrario al recibirlo, osea volverlo una estructura de datos
+    //de nuevo 
+    echo json_encode($panes);
+?>
